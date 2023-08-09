bfe60bee2807dcbbd9a732b1afdaf4dfe9bb6763
aefshaeful <aefsaefulrohma042@gmail.com>
2023-08-09 22:53:45 +0700

    refactor: create department model

--- API/Models/Department.cs
+++ API/Models/Department.cs
@@ -1,6 +1,16 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_departments")]
+public class Department : BaseEntity
 {
-    public class Department
-    {
-    }
+    [Column("name", TypeName = "nvarchar(30)")]
+    public string Name { get; set; }
+
+    [Column("manager_guid")]
+    public Guid ManagerGuid { get; set; }
+
+    [Column("job_guid")]
+    public Guid JobGuid { get; set; }
 }

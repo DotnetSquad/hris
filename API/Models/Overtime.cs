8c9ac867a280ad434b9b24783a98493a05167ba4
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-08-09 23:51:06 +0700

    create overtime model (#32)

---
+++ API/Models/Overtime.cs
@@ -0,0 +1,21 @@
+﻿using API.Utilities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_overtimes")]
+public class Overtime : BaseEntity
+{
+    [Column("request_number")]
+    public int RequestNumber { get; set; }
+    [Column("submited_date")]
+    public DateTime SubmitedDate { get; set; }
+    [Column("remaks", TypeName = "nvarchar(255)")]
+    public string Remaks { get; set; }
+    [Column("status")]
+    public StatusEnum Status { get; set; }
+    [Column("remaining")]
+    public int Remaining { get; set; }
+    [Column("employee_guid")]
+    public Guid EmployeeGuid { get; set; }
+}

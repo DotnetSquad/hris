27cc9047e36d612b43a94dc355eaf69d659b561e
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-08-09 22:55:18 +0700

    feat: create department model (#24)
    * feat: create department model
    
    * refactor: create department model

---
+++ API/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_departments")]
+public class Department : BaseEntity
+{
+    [Column("name", TypeName = "nvarchar(30)")]
+    public string Name { get; set; }
+
+    [Column("manager_guid")]
+    public Guid ManagerGuid { get; set; }
+
+    [Column("job_guid")]
+    public Guid JobGuid { get; set; }
+}

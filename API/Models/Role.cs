ac75789bf4642cb7f799f11bbf002635768395b8
Rifky Maulana <rifkymaulanaindonesia@gmail.com>
2023-08-09 22:35:47 +0700

    feat: create role model (#21)

---
+++ API/Models/Role.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_roles")]
+public class Role : BaseEntity
+{
+    [Column("name", TypeName = "nvarchar(100)")]
+    public string Name { get; set; }
+}

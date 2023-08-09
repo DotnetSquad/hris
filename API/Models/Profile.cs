3ebda36229ab98441358cbed2de1e4b1494fe7dd
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-08-09 22:34:50 +0700

    feat: create profile model (#20)
    * feat: create profile model
    
    * feat: update profile models

---
+++ API/Models/Profile.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_profiles")]
+public class Profile : BaseEntity
+{
+    [Column("photo")] public string Photo { get; set; }
+}

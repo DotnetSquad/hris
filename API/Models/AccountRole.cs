9cbf19cc23fb95205fe4cda754985e596157237a
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-08-09 22:45:36 +0700

    create account role model adn update profile model

---
+++ API/Models/AccountRole.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+public class AccountRole : BaseEntity
+{
+    [Column("account_guid")] public Guid AccountGuid { get; set; }
+
+    [Column("role_guid")] public Guid RoleGuid { get; set; }
+}

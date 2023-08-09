5539c6eeb3c19ceff25b0e0c1e00ec8c58b63279
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-08-09 23:10:55 +0700

    update profile, base entity and ibaserepository

--- API/Models/AccountRole.cs
+++ API/Models/AccountRole.cs
@@ -5,7 +5,9 @@ namespace API.Models;
 [Table("tb_m_account_roles")]
 public class AccountRole : BaseEntity
 {
-    [Column("account_guid")] public Guid AccountGuid { get; set; }
+    [Column("account_guid")] 
+    public Guid AccountGuid { get; set; }
 
-    [Column("role_guid")] public Guid RoleGuid { get; set; }
+    [Column("role_guid")] 
+    public Guid RoleGuid { get; set; }
 }
--- API/Models/BaseEntity.cs
+++ API/Models/BaseEntity.cs
@@ -4,7 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API.Models;
 public abstract class BaseEntity 
 {
-    [Key] [Column("guid")] public Guid Guid {get; set;}
-    [Column("created_date")] public DateTime CreatedDate { get; set; }
-    [Column("modified_date")] public DateTime ModifiedDate { get; set; }
+    [Key] [Column("guid")] 
+    public Guid Guid {get; set;}
+    [Column("created_date")] 
+    public DateTime CreatedDate { get; set; }
+    [Column("modified_date")] 
+    public DateTime ModifiedDate { get; set; }
 }

6eb8f25dcf62c03726d69c3c14dabf1f81e33966
putuagra <iputuagra@gmail.com>
2023-08-10 09:39:28 +0700

    feat: add cardinality role model (#48)

--- API/Models/Role.cs
+++ API/Models/Role.cs
@@ -7,4 +7,7 @@ public class Role : BaseEntity
 {
     [Column("name", TypeName = "nvarchar(100)")]
     public string Name { get; set; }
+
+    // Cardinality
+    public ICollection<AccountRole>? AccountRoles { get; set; }
 }

2ceb79607e4b3b637a47a8c4f07451658d0aee41
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-08-10 10:20:10 +0700

    feat: add cardinality jobs model (#52)
    * feat: add cardinality jobs model
    
    * feat: update cardinality jobs model and update employee cardinality

--- API/Models/Job.cs
+++ API/Models/Job.cs
@@ -5,9 +5,17 @@ namespace API.Models;
 [Table("tb_m_jobs")]
 public class Job : BaseEntity
 {
+
     [Column("position", TypeName = "nvarchar(100)")]
     public string Position { get; set; }
     
     [Column("salary")]
     public int Salary { get; set; }
+
+    [Column("department_guid")]
+    public Guid DepartmentGuid { get; set; }
+
+    // Cardinality
+    public Department? Department { get; set; }
+    public Employee? Employee { get; set; }
 }

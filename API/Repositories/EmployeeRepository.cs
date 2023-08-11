83abc6edbd15ed5e89510d79ead3b6482de97ff9
Wahyu Zul Fahmi <Wahyuzulfahmi14@gmail.com>
2023-08-11 14:09:57 +0700

    feat: create employee repository

---
+++ API/Repositories/EmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
+{
+    protected EmployeeRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}

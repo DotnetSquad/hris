fc1ec8f75aced45ec750d5c98673b5ec51920b72
aefshaeful <aefsaefulrohma042@gmail.com>
2023-08-11 15:23:31 +0700

    feat: create role dto

---
+++ API/DataTransferObjects/Roles/RoleDtoGet.cs
@@ -0,0 +1,29 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Roles;
+
+public class RoleDtoGet
+{
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+
+    // implicit operator
+    public static implicit operator Role(RoleDtoGet roleDtoGet)
+    {
+        return new Role
+        {
+            Guid = roleDtoGet.Guid,
+            Name = roleDtoGet.Name
+        };
+    }
+
+    // explicit operator
+    public static explicit operator RoleDtoGet(Role role)
+    {
+        return new RoleDtoGet
+        {
+            Guid = role.Guid,
+            Name = role.Name
+        };
+    }
+}

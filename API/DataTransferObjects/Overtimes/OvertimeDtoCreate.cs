bd8c40f2071f9202519927ad6afe332cbea4a6c6
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-08-11 20:03:50 +0700

    feat: create overtime dto and update model

---
+++ API/DataTransferObjects/Overtimes/OvertimeDtoCreate.cs
@@ -0,0 +1,39 @@
+﻿using API.Models;
+using API.Utilities.Enums;
+
+namespace API.DataTransferObjects.Overtimes;
+
+public class OvertimeDtoCreate
+{
+    public DateTime SubmitedDate { get; set; }
+    public string Remarks { get; set; }
+    public StatusEnum Status { get; set; }
+    public int Remaining { get; set; }
+    public Guid EmployeeGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator Overtime(OvertimeDtoCreate overtimeDtoCreate)
+    {
+        return new Overtime
+        {
+            SubmitedDate = overtimeDtoCreate.SubmitedDate,
+            Remarks = overtimeDtoCreate.Remarks,
+            Status = overtimeDtoCreate.Status,
+            Remaining = overtimeDtoCreate.Remaining,
+            EmployeeGuid = overtimeDtoCreate.EmployeeGuid
+        };
+    }
+
+    // expilicit operator
+    public static explicit operator OvertimeDtoCreate(Overtime overtime)
+    {
+        return new OvertimeDtoCreate
+        {
+            SubmitedDate = overtime.SubmitedDate,
+            Remarks = overtime.Remarks,
+            Status = overtime.Status,
+            Remaining = overtime.Remaining,
+            EmployeeGuid = overtime.EmployeeGuid
+        };
+    }
+}
---
+++ API/DataTransferObjects/Overtimes/OvertimeDtoGet.cs
@@ -0,0 +1,45 @@
+﻿using API.Models;
+using API.Utilities.Enums;
+
+namespace API.DataTransferObjects.Overtimes;
+
+public class OvertimeDtoGet
+{
+    public Guid Guid { get; set; }
+    public int RequestNumber { get; set; }
+    public DateTime SubmitedDate { get; set; }
+    public string Remarks { get; set; }
+    public StatusEnum Status { get; set; }
+    public int Remaining { get; set; }
+    public Guid EmployeeGuid { get; set; }
+
+    // Implicit operator
+    public static implicit operator Overtime(OvertimeDtoGet overtimeDtoGet)
+    {
+        return new Overtime
+        {
+            Guid = overtimeDtoGet.Guid,
+            RequestNumber = overtimeDtoGet.RequestNumber,
+            SubmitedDate = overtimeDtoGet.SubmitedDate,
+            Remarks = overtimeDtoGet.Remarks,
+            Status = overtimeDtoGet.Status,
+            Remaining = overtimeDtoGet.Remaining,
+            EmployeeGuid = overtimeDtoGet.EmployeeGuid
+        };
+    }
+
+    // Explicit operator
+    public static explicit operator OvertimeDtoGet(Overtime overtime)
+    {
+        return new OvertimeDtoGet
+        {
+            Guid = overtime.Guid,
+            RequestNumber = overtime.RequestNumber,
+            SubmitedDate = overtime.SubmitedDate,
+            Remarks = overtime.Remarks,
+            Status = overtime.Status,
+            Remaining = overtime.Remaining,
+            EmployeeGuid = overtime.EmployeeGuid
+        };
+    }
+}

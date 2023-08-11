b30203ce6a69ded79eb51455bdf805ec364545a9
Kherogen <rayvaldoprawira@gmail.com>
2023-08-11 21:19:31 +0700

    feat: create job dto

---
+++ API/DataTransferObjects/Jobs/JobDtoCreate.cs
@@ -0,0 +1,34 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Jobs;
+
+public class JobDtoCreate
+{
+    public string Position { get; set; }
+
+    public int Salary { get; set; }
+
+    public Guid DepeartmentGuid { get; set; }
+
+    //Implicit Operator
+    public static implicit operator Job(JobDtoCreate jobDtoCreate)
+    {
+        return new Job
+        {
+            Position = jobDtoCreate.Position,
+            Salary = jobDtoCreate.Salary,
+            DepartmentGuid = jobDtoCreate.DepeartmentGuid
+        };
+    }
+
+    //Explicit Opearator
+    public static explicit operator JobDtoCreate(Job job)
+    {
+        return new JobDtoCreate
+        {
+            Position = job.Position,
+            Salary = job.Salary,
+            DepeartmentGuid = job.DepartmentGuid
+        };
+    }
+}

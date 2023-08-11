3600c2a9f1ad309e031ea9bdb569b90e84771b38
aefshaeful <aefsaefulrohma042@gmail.com>
2023-08-11 14:24:57 +0700

    feat: create overtime repository

---
+++ API/Repositories/OvertimeRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class OvertimeRepository : BaseRepository<Overtime>, IOvertimeRepository
+{
+    public OvertimeRepository(ApplicationDbContext context) : base(context) { }
+}

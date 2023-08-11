a31a1b7f6a85e52fecb8db75b4639e153a74cb1e
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-08-11 14:35:54 +0700

    feat: create account role repository

---
+++ API/Repositories/AccountRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class AccountRoleRepository : BaseRepository<AccountRole>, IAccountRoleRepository
+{
+    public AccountRoleRepository(ApplicationDbContext context) : base(context) { }
+}

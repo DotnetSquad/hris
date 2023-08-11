91f46e5947f9671e2b28b14c690059adda153a71
Wahyu Zul Fahmi <Wahyuzulfahmi14@gmail.com>
2023-08-11 17:20:41 +0700

    feat: create account role dto

---
+++ API/DataTransferObjects/AccountRoles/AccountRoleDtoUpdate.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.AccountRoles;
+
+public class AccountRoleDtoUpdate
+{
+    public Guid Guid { get; set; }
+    public Guid AccountGuid { get; set; }
+    public Guid RoleGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator AccountRole(AccountRoleDtoUpdate accountRoleDtoUpdate)
+    {
+        return new AccountRole
+        {
+            Guid = accountRoleDtoUpdate.Guid,
+            AccountGuid = accountRoleDtoUpdate.AccountGuid,
+            RoleGuid = accountRoleDtoUpdate.RoleGuid,
+            ModifiedDate = DateTime.UtcNow
+        };
+    }
+
+    // explicit operator
+    public static explicit operator AccountRoleDtoUpdate(AccountRole accountRole)
+    {
+        return new AccountRoleDtoUpdate
+        {
+            Guid = accountRole.Guid,
+            AccountGuid = accountRole.AccountGuid,
+            RoleGuid = accountRole.RoleGuid
+        };
+    }
+}

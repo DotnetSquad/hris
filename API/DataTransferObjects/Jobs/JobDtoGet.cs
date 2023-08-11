7c6b48c1c6a2621fad37f5fff605909bf1d16385
Rayvaldo <79625064+rayvaldoprawira@users.noreply.github.com>
2023-08-11 21:27:28 +0700

    feat: create job dto (#120)
    * feat: create job dto
    
    * Update JobDtoCreate.cs
    
    * Update JobDtoCreate.cs
    
    ---------
    
    Co-authored-by: Rifky Maulana <rifkymaulanaindonesia@gmail.com>

---
+++ API/DataTransferObjects/Jobs/JobDtoGet.cs
@@ -0,0 +1,38 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Jobs;
+
+public class JobDtoGet
+{
+    public Guid Guid { get; set; }
+
+    public string Position { get; set; }
+
+    public int Salary { get; set; }
+
+    public Guid DepeartmentGuid { get; set; }
+
+    //Implicit Operator
+    public static implicit operator Job(JobDtoGet jobDtoGet)
+    {
+        return new Job
+        {
+            Guid = jobDtoGet.Guid,
+            Position = jobDtoGet.Position,
+            Salary = jobDtoGet.Salary,
+            DepartmentGuid = jobDtoGet.DepeartmentGuid
+        };
+    }
+
+    //Explicit Opearator
+    public static explicit operator JobDtoGet(Job job)
+    {
+        return new JobDtoGet
+        {
+            Guid = job.Guid,
+            Position = job.Position,
+            Salary = job.Salary,
+            DepeartmentGuid = job.DepartmentGuid
+        };
+    }
+}
---
+++ API/DataTransferObjects/Jobs/JobDtoUpdate.cs
@@ -0,0 +1,38 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Jobs;
+
+public class JobDtoUpdate
+{
+    public Guid Guid { get; set; }
+
+    public string Position { get; set; }
+
+    public int Salary { get; set; }
+
+    public Guid DepeartmentGuid { get; set; }
+
+    //Implicit Operator
+    public static implicit operator Job(JobDtoUpdate jobDtoUpdate)
+    {
+        return new Job
+        {
+            Guid = jobDtoUpdate.Guid,
+            Position = jobDtoUpdate.Position,
+            Salary = jobDtoUpdate.Salary,
+            DepartmentGuid = jobDtoUpdate.DepeartmentGuid
+        };
+    }
+
+    //Explicit Opearator
+    public static explicit operator JobDtoUpdate(Job job)
+    {
+        return new JobDtoUpdate
+        {
+            Guid = job.Guid,
+            Position = job.Position,
+            Salary = job.Salary,
+            DepeartmentGuid = job.DepartmentGuid
+        };
+    }
+}

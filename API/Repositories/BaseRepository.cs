689e974e0042a3fc435ab5ec03844846509a10b4
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-08-11 12:32:44 +0700

    feat: create base repository

---
+++ API/Repositories/BaseRepository.cs
@@ -0,0 +1,77 @@
+﻿using API.Contracts;
+using API.Data;
+
+namespace API.Repositories;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    protected readonly ApplicationDbContext _context;
+
+    protected BaseRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<TEntity> GetAll()
+    {
+        return _context.Set<TEntity>().ToList();
+    }
+
+    public TEntity? GetByGuid(Guid guid)
+    {
+        var entity = _context.Set<TEntity>().Find(guid);
+        _context.ChangeTracker.Clear();
+        return entity;
+    }
+
+    public TEntity Create(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null!;
+        }
+    }
+
+    public bool Update(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Update(entity);
+            _context.SaveChanges();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+
+    public bool Delete(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+
+    public bool IsExist(Guid guid)
+    {
+        return _context.Set<TEntity>().Find(guid) != null;
+    }
+
+}

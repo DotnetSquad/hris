843b6ab4d6066e5edb701306f4db27f4323260ab
putuagra <iputuagra@gmail.com>
2023-08-11 14:02:08 +0700

    feat: create role repository (#94)

---
+++ API/Repositories/RoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class RoleRepository : BaseRepository<Role>, IRoleRepository
+{
+    public RoleRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}

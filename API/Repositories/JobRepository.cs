8f1416a99e7d22b54402bfb7f402c9299274f7ed
ihsannurmaulana <81961193+ihsannurmaulana@users.noreply.github.com>
2023-08-11 14:02:34 +0700

    feat: create job repository (#93)

---
+++ API/Repositories/JobRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class JobRepository : BaseRepository<Job>, IJobRepository
+{
+    public JobRepository(ApplicationDbContext context) : base(context) { }
+}

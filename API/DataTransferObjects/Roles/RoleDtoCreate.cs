5c11263b2873389cca811dc5031f1ce9dd685ed9
aefshaeful <105156900+aefshaeful@users.noreply.github.com>
2023-08-11 15:43:02 +0700

    feat: create role dto (#109)

---
+++ API/DataTransferObjects/Roles/RoleDtoCreate.cs
@@ -0,0 +1,27 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Roles;
+
+public class RoleDtoCreate
+{
+    public string Name { get; set; }
+
+    // implicit operator
+    public static implicit operator Role(RoleDtoCreate roleDtoCreate)
+    {
+        return new Role
+        {
+            Name = roleDtoCreate.Name,
+            CreatedDate = DateTime.UtcNow
+        };
+    }
+
+    // explicit operator
+    public static explicit operator RoleDtoCreate(Role role)
+    {
+        return new RoleDtoCreate
+        {
+            Name = role.Name
+        };
+    }
+}
---
+++ API/DataTransferObjects/Roles/RoleDtoUpdate.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.Roles;
+
+public class RoleDtoUpdate
+{
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+
+    // implicit operator
+    public static implicit operator Role(RoleDtoUpdate roleDtoUpdate)
+    {
+        return new Role
+        {
+            Guid = roleDtoUpdate.Guid,
+            Name = roleDtoUpdate.Name,
+            ModifiedDate = DateTime.UtcNow
+        };
+    }
+
+    // explicit operator
+    public static explicit operator RoleDtoUpdate(Role role)
+    {
+        return new RoleDtoUpdate
+        {
+            Guid = role.Guid,
+            Name = role.Name
+        };
+    }
+}

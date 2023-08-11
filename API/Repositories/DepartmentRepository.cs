3bc81b46ca02adc35db9c30a5d113d542c4d0664
ihsannurmaulana <81961193+ihsannurmaulana@users.noreply.github.com>
2023-08-11 14:45:31 +0700

    feat: create department repository (#100)

---
+++ API/Repositories/DepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class DepartmentRepository : BaseRepository<Department>, IDepartmentRepository
+{
+    public DepartmentRepository(ApplicationDbContext context) : base(context) { }
+}

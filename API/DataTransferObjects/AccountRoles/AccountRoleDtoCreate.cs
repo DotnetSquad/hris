46651f4398c02f39c253e6e12850de648e7b1e70
Wahyu Zul Fahmi <42335797+Wahyuzul@users.noreply.github.com>
2023-08-11 17:59:34 +0700

    feat: create account role dto (#113)

---
+++ API/DataTransferObjects/AccountRoles/AccountRoleDtoCreate.cs
@@ -0,0 +1,32 @@
+﻿using API.DataTransferObjects.Departments;
+using API.Models;
+
+namespace API.DataTransferObjects.AccountRoles;
+
+public class AccountRoleDtoCreate
+{
+    public Guid AccountGuid { get; set; }
+    public Guid RoleGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator AccountRole(AccountRoleDtoCreate accountRoleDtoCreate)
+    {
+        return new AccountRole
+        {
+            AccountGuid = accountRoleDtoCreate.AccountGuid,
+            RoleGuid = accountRoleDtoCreate.RoleGuid,
+            CreatedDate = DateTime.UtcNow
+        };
+    }
+
+    // explicit operator
+    public static explicit operator AccountRoleDtoCreate(AccountRole accountRole)
+    {
+        return new AccountRoleDtoCreate
+        {
+            AccountGuid = accountRole.AccountGuid,
+            RoleGuid = accountRole.RoleGuid
+        };
+    }
+
+}
---
+++ API/DataTransferObjects/AccountRoles/AccountRoleDtoGet.cs
@@ -0,0 +1,32 @@
+﻿using API.Models;
+
+namespace API.DataTransferObjects.AccountRoles;
+
+public class AccountRoleDtoGet
+{
+    public Guid Guid { get; set; }
+    public Guid AccountGuid { get; set; }
+    public Guid RoleGuid { get; set; }
+
+    // implicit operator
+    public static implicit operator AccountRole(AccountRoleDtoGet accountRoleDtoGet)
+    {
+        return new AccountRole
+        {
+            Guid = accountRoleDtoGet.Guid,
+            AccountGuid = accountRoleDtoGet.AccountGuid,
+            RoleGuid = accountRoleDtoGet.RoleGuid
+        };
+    }
+
+    // explicit operator
+    public static explicit operator AccountRoleDtoGet(AccountRole accountRole)
+    {
+        return new AccountRoleDtoGet
+        {
+            Guid = accountRole.Guid,
+            AccountGuid = accountRole.AccountGuid,
+            RoleGuid = accountRole.RoleGuid
+        };
+    }
+}

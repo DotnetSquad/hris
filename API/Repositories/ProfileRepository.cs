f7eb87943a1be33d80df89b6d27e4a9a9e94c03c
putuagra <iputuagra@gmail.com>
2023-08-11 14:01:46 +0700

    feat: create profile repository (#95)

---
+++ API/Repositories/ProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class ProfileRepository : BaseRepository<Profile>, IProfileRepository
+{
+    public ProfileRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}

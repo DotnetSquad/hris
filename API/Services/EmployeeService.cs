c442f2abf2d40fb620ca3445044f00cdf5874e2c
aefshaeful <aefsaefulrohma042@gmail.com>
2023-08-14 14:16:49 +0700

    feat: create employee service

---
+++ API/Services/EmployeeService.cs
@@ -0,0 +1,68 @@
+﻿using API.Contracts;
+using API.DataTransferObjects.Employees;
+using API.Models;
+
+namespace API.Services;
+
+public class EmployeeService
+{
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public EmployeeService(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    public IEnumerable<EmployeeDtoGet> Get()
+    {
+        var employees = _employeeRepository.GetAll().ToList();
+        if (!employees.Any()) return Enumerable.Empty<EmployeeDtoGet>();
+        List<EmployeeDtoGet> employeeDtoGets = new();
+
+        foreach (var employee in employees)
+        {
+            employeeDtoGets.Add
+            ((EmployeeDtoGet)employee);
+        }
+
+        return employeeDtoGets;
+    }
+
+    public EmployeeDtoGet? Get(Guid guid)
+    {
+        var employee = _employeeRepository.GetByGuid(guid);
+        if (employee is null) return null!;
+
+        return (EmployeeDtoGet)employee;
+    }
+
+    public EmployeeDtoCreate? Create(EmployeeDtoCreate employeeDtoCreate)
+    {
+        Employee employee = employeeDtoCreate;
+        /*employee.Nik = GenerateHandler.Nik(_employeeRepository.GetLastEmpoyeeNik());*/
+
+        var createdEmployee = _employeeRepository.Create(employee);
+
+        if (createdEmployee is null) return null; 
+
+        return (EmployeeDtoCreate)createdEmployee;
+    }
+
+    public int Update(EmployeeDtoUpdate employeeDtoUpdate)
+    {
+        var employee = _employeeRepository.GetByGuid(employeeDtoUpdate.Guid);
+        if (employee is null) return -1;
+
+        var employeeUpdated = _employeeRepository.Update(employeeDtoUpdate);
+        return !employeeUpdated ? 0 : 1;
+    }
+
+    public int Delete(Guid guid)
+    {
+        var employee = _employeeRepository.GetByGuid(guid);
+        if (employee is null) return -1;
+
+        var employeeDeleted = _employeeRepository.Delete(employee);
+        return employeeDeleted ? 1 : 0;
+    }
+}

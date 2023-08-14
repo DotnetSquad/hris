282c418325f1e9d417bf439adab7b8a46c9aa795
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-08-14 17:03:41 +0700

    update: response handler

--- API/Utilities/Handlers/ResponseHandler.cs
+++ API/Utilities/Handlers/ResponseHandler.cs
@@ -1,10 +1,9 @@
-﻿namespace API.Utilities.Handlers
+﻿namespace API.Utilities.Handlers;
+
+public class ResponseHandler<TEntity>
 {
-    public class ResponseHandler<TEntity>
-    {
-        public int Code { get; set; }
-        public string Status { get; set; }
-        public string Message { get; set; }
-        public TEntity? Data { get; set; } 
-    }
+    public int Code { get; set; }
+    public string Status { get; set; }
+    public string Message { get; set; }
+    public TEntity? Data { get; set; } 
 }

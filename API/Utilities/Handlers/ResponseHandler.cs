fa0bfd41680cf5aeb4bf0b537839de47b06f3f32
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-08-14 17:05:27 +0700

    feat: create response handler (#145)
    * feat: create response handler
    
    * update: response handler

---
+++ API/Utilities/Handlers/ResponseHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Utilities.Handlers;
+
+public class ResponseHandler<TEntity>
+{
+    public int Code { get; set; }
+    public string Status { get; set; }
+    public string Message { get; set; }
+    public TEntity? Data { get; set; } 
+}

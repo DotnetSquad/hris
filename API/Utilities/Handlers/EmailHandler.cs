97cbdefcf1174da1f63fe2241d73f88f571b8b0c
ihsannurmaulana <81961193+ihsannurmaulana@users.noreply.github.com>
2023-08-15 16:22:51 +0700

    feat: create email handler (#164)

---
+++ API/Utilities/Handlers/EmailHandler.cs
@@ -0,0 +1,44 @@
+﻿using API.Contracts;
+using System.Net;
+using System.Net.Mail;
+
+namespace API.Utilities.Handlers;
+
+public class EmailHandler : IEmailHandler
+{
+    private readonly string _smtpServer;
+    private readonly int _smtpPort;
+    private readonly string _fromEmailAddress;
+    private readonly string _fromEmailPassword;
+
+    public EmailHandler(string smtpServer, int smtpPort, string fromEmailAddress, string fromEmailPassword)
+    {
+        _smtpServer = smtpServer;
+        _smtpPort = smtpPort;
+        _fromEmailAddress = fromEmailAddress;
+        _fromEmailPassword = fromEmailPassword;
+    }
+
+    public void sendEmail(string toEmail, string subject, string htmlMessage)
+    {
+        var message = new MailMessage
+        {
+            From = new MailAddress(_fromEmailAddress),
+            Subject = subject,
+            Body = htmlMessage,
+            IsBodyHtml = true
+        };
+
+        message.To.Add(new MailAddress(toEmail));
+
+        var client = new SmtpClient(_smtpServer)
+        {
+            Port = _smtpPort,
+            //UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(_fromEmailAddress, _fromEmailPassword),
+            EnableSsl = true,
+        };
+
+        client.Send(message);
+    }
+}
